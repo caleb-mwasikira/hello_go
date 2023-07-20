8d56a1e1b284c4ac8b68adff34a4211b4cd50809
caleb_mwasikira <calebmwasikira@gmail.com>
2023-07-20 15:22:35 +0300

    modules, packages and panics

--- main.go
+++ main.go
@@ -2,33 +2,27 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+
+	"example.com/hello_go/calc"
 )
 
 func main() {
-	fmt.Println("Hello Go!")
+	var numbers = []float64{1, 2, 3, 4, 5}
 
-	var (
-		max       uint64 = 10
-		min       uint64 = 1
-		str_input string
-		number    uint64
-	)
+	fmt.Printf("Array: %v\nAverage: %v\n", numbers, calc.Average(numbers...))
+	fmt.Printf("Std Deviation: %v\n", calc.StandardDeviation(numbers...))
 
-	for {
-		fmt.Printf("Enter a number between %d-%d: ", min, max)
-		fmt.Scanln(&str_input)
-
-		var err error
-		number, err = strconv.ParseUint(str_input, 10, 64)
-		if err != nil || (number < min || number > max) {
-			fmt.Printf("Number MUST be between %d-%d\n", min, max)
-			continue
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("panic occurred:", err)
 		}
+	}()
 
-		break
-	}
+	number := 5
+	iterative_fact := calc.IterativeFactorial(number)
+	recursive_fact := calc.RecursiveFactorial(number)
 
-	fmt.Printf("You entered the number: %v\n", number)
+	fmt.Printf("Iterative Factorial: %v! = %v\n", number, iterative_fact)
+	fmt.Printf("Recursive Factorial: %v! = %v\n", number, recursive_fact)
 	return
 }
