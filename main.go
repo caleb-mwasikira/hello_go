77f949e5b68e782d87834378b222081524fd6b45
Caleb Mwasikira <calebmwasikira@gmail.com>
2024-08-04 00:27:41 +0300

    stack data structure

--- main.go
+++ main.go
@@ -1,70 +1,84 @@
 package main
 
 import (
-	"bytes"
-	"compress/gzip"
-	"crypto/md5"
-	"crypto/rand"
-	"encoding/hex"
+	"errors"
 	"fmt"
-	"io"
+	"log"
 )
 
-func hashData(data []byte) string {
-	b_hash := md5.New().Sum(data)[:20]
-	return hex.EncodeToString(b_hash)
+var (
+	ErrStackFull        error = errors.New("stack already full")
+	ErrStackEmpty       error = errors.New("stack is empty")
+	ErrInvalidStackSize error = errors.New("stack size must be greater than one")
+)
+
+type Stack struct {
+	items []int
+	top   int
+	size  int
 }
 
-func printSummary(data []byte) {
-	fmt.Println("Length: ", len(data))
-	fmt.Println("Hash: ", hashData(data))
+func NewStack(size int, items ...int) (Stack, error) {
+	if size <= 0 {
+		return Stack{}, ErrInvalidStackSize
+	}
+
+	return Stack{
+		items: make([]int, size),
+		top:   -1,
+		size:  size,
+	}, nil
+}
+
+func (s *Stack) push(items ...int) (bool, error) {
+	for _, item := range items {
+		if s.top == s.size-1 {
+			return false, ErrStackFull
+		}
+
+		s.top++
+		s.items[s.top] = item
+	}
+	return true, nil
+}
+
+func (s *Stack) pop() (int, error) {
+	if s.top == -1 {
+		return 0, ErrStackEmpty
+	}
+
+	item := s.items[s.top]
+	s.top--
+	return item, nil
+}
+
+func (s Stack) getItems() []int {
+	items := s.items[0 : s.top+1]
+	return items
 }
 
 func main() {
-	// Data to be compressed
-	data := make([]byte, 1024)
-	_, err := rand.Read(data)
+	stack, err := NewStack(10)
 	if err != nil {
-		fmt.Printf("Error generating random bytes; %v\n", err)
-		return
+		log.Fatal(err)
 	}
 
-	fmt.Println("Before compression: ")
-	printSummary(data)
+	for i := 5; i < 20; i++ {
+		_, err = stack.push(i)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	// Create a buffer to store compressed data
-	var buf bytes.Buffer
-
-	// Any data written to gzip writer is automatically compressed
-	gw := gzip.NewWriter(&buf)
-	_, err = gw.Write(data)
-	if err != nil {
-		fmt.Println("Error during compression:", err)
-		return
+		fmt.Printf("stack: %#v\n", stack.getItems())
 	}
 
-	// Close the writer to flush any remaining data
-	if err := gw.Close(); err != nil {
-		fmt.Println("Error closing gzip writer:", err)
-		return
+	for i := 0; i < 2; i++ {
+		val, err := stack.pop()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("popped value: %v\n", val)
+		fmt.Printf("stack: %#v\n", stack.getItems())
 	}
-
-	fmt.Println("After compression: ")
-	printSummary(buf.Bytes())
-
-	// Decompress the data (optional)
-	reader, err := gzip.NewReader(&buf)
-	if err != nil {
-		fmt.Println("error initialising reader:", err)
-		return
-	}
-
-	decompressed, err := io.ReadAll(reader)
-	if err != nil {
-		fmt.Println("Error during decompression:", err)
-		return
-	}
-
-	fmt.Println("After decompresion: ")
-	printSummary(decompressed)
 }
