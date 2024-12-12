36d4b2ff24638497545dea701e48affa5eb75791
Caleb Mwasikira <calebmwasikira@gmail.com>
2024-12-13 01:11:41 +0300

    concurrent summation of elements of arrays

--- main.go
+++ main.go
@@ -1,84 +1,65 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"log"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
 )
 
-var (
-	ErrStackFull        error = errors.New("stack already full")
-	ErrStackEmpty       error = errors.New("stack is empty")
-	ErrInvalidStackSize error = errors.New("stack size must be greater than one")
-)
+func genRandomArray(size int) []int {
+	nums := []int{}
 
-type Stack struct {
-	items []int
-	top   int
-	size  int
-}
-
-func NewStack(size int, items ...int) (Stack, error) {
-	if size <= 0 {
-		return Stack{}, ErrInvalidStackSize
+	for i := 0; i < size; i++ {
+		randNum := rand.Intn(10)
+		nums = append(nums, randNum)
 	}
 
-	return Stack{
-		items: make([]int, size),
-		top:   -1,
-		size:  size,
-	}, nil
+	return nums
 }
 
-func (s *Stack) push(items ...int) (bool, error) {
-	for _, item := range items {
-		if s.top == s.size-1 {
-			return false, ErrStackFull
-		}
-
-		s.top++
-		s.items[s.top] = item
+func calcSumOfArray(nums []int) int {
+	total := 0
+	for _, value := range nums {
+		total += value
 	}
-	return true, nil
-}
-
-func (s *Stack) pop() (int, error) {
-	if s.top == -1 {
-		return 0, ErrStackEmpty
-	}
-
-	item := s.items[s.top]
-	s.top--
-	return item, nil
-}
-
-func (s Stack) getItems() []int {
-	items := s.items[0 : s.top+1]
-	return items
+	return total
 }
 
 func main() {
-	stack, err := NewStack(10)
-	if err != nil {
-		log.Fatal(err)
+	var (
+		seq_total  int   = 0
+		conc_total int64 = 0
+	)
+
+	nums := genRandomArray(1000000)
+
+	start := time.Now()
+	seq_total = calcSumOfArray(nums)
+	duration := time.Since(start)
+
+	fmt.Printf("sequential total: %v \t in %v\n", seq_total, duration)
+
+	// concurrent soln
+	start = time.Now()
+	offset := 10000
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < len(nums); i += offset {
+		begin := i
+		end := i + offset
+		smaller_arr := nums[begin:end]
+
+		wg.Add(1)
+		go func(smaller_arr []int) {
+			defer wg.Done()
+			small_total := calcSumOfArray(smaller_arr)
+			atomic.AddInt64(&conc_total, int64(small_total))
+		}(smaller_arr)
 	}
+	wg.Wait()
+	duration = time.Since(start)
 
-	for i := 5; i < 20; i++ {
-		_, err = stack.push(i)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("stack: %#v\n", stack.getItems())
-	}
-
-	for i := 0; i < 2; i++ {
-		val, err := stack.pop()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("popped value: %v\n", val)
-		fmt.Printf("stack: %#v\n", stack.getItems())
-	}
+	fmt.Printf("concurrent total: %v \t in %v\n", conc_total, duration)
 }
